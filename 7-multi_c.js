d01d96c3f26024dae5bdf9281a7bb44cf2756cce
Tebogocodes25 <sitholet227@gmail.com>
2025-05-23 23:14:58 +0200

    Add script for printing 'C is fun' multiple times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,14 @@
+#!/usr/bin/node
 
-const x = parseInt(process.argv[2], 10); // Convert argument to an integer
+const num = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
-    console.log("Missing number of occurrences");
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < x; i++) {
-        console.log("C is fun");
-    }
+  let i = 0;
+  while (i < num) {
+    console.log('C is fun');
+    i++;
+  }
 }
+
