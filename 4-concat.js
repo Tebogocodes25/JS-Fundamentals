f955f80843833d177ddc4f648641d7b495ec2303
Tebogocodes25 <sitholet227@gmail.com>
2025-05-23 15:24:24 +0200

    Add script for concatenating arguments

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,5 @@
+
 const firstArg = process.argv[2]; // First argument from the command line
 const secondArg = process.argv[3]; // Second argument from the command line
 
 console.log(firstArg + " is " + secondArg);
-C is fun
